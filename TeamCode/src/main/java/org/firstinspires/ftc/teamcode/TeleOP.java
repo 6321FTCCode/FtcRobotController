f88084a3c5a275b1dcf85d16f4174baa0abb3be3
6321FTCCode <santiago.vasquez@aquinas-sta.org>
2025-09-17 15:22:00 -0400

    attempted an auto and added some stuff to TeleOP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TeleOP extends OpMode {
+
+    @Override
+    public void init() {
+        //code that runs once when INIT is pressed
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+        //code that runs repeat
+        if (gamepad1.a){
+            telemetry.addData("A button Pressed",gamepad1.a);
+            telemetry.update();
+        }
+    }
+
+}
