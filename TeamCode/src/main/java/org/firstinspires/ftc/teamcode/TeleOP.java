9136f3c6d74e6f67afb75c83851c10abfc22da68
6321FTCCode <santiago.vasquez@aquinas-sta.org>
2025-09-17 16:00:01 -0400

    Update TeleOP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -12,15 +12,18 @@ public class TeleOP extends OpMode {
     @Override
     public void init() {
         //code that runs once when INIT is pressed
-        telemetry.addData("Hello", "World");
+        motor  = hardwareMap.get(DcMotor.class, "motor");
+        telemetry.addData("Hardware: ", "Initialized");
     }
 
     @Override
     public void loop() {
         //code that runs repeat
+
         if (gamepad1.a){
             telemetry.addData("A button Pressed",gamepad1.a);
             telemetry.update();
+            motor.setPower(0.5);
         }
     }
 
