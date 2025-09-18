23ab3142b36b4c303cbae7396ee8536be7d37d91
6321FTCCode <santiago.vasquez@aquinas-sta.org>
2025-09-18 15:53:13 -0400

    Update TeleOP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -6,26 +6,50 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp (name = "main")
 public class TeleOP extends OpMode {
-
-    DcMotor motor;
+    double ticks = 537.7;
+    double newTarget;
+    DcMotor motorFR;
+    DcMotor motorFL;
+    DcMotor motorBR;
+    DcMotor motorBL;
 
     @Override
     public void init() {
         //code that runs once when INIT is pressed
-        motor  = hardwareMap.get(DcMotor.class, "motor");
+        motorFR  = hardwareMap.get(DcMotor.class, "motor");
+//        motorFL  = hardwareMap.get(DcMotor.class, "motorFL");
+//        motorBR  = hardwareMap.get(DcMotor.class, "motorBR");
+//        motorBL  = hardwareMap.get(DcMotor.class, "motorBL");
         telemetry.addData("Hardware: ", "Initialized");
+
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
         //code that runs repeat
+        double speed = 0.5;
+        float lStick = gamepad1.left_stick_y;
+//        float lTrigger = gamepad1.left_trigger;
+//        float rTrigger = gamepad1.right_trigger;
+//        if (lStick<0){
+//            motorFR.setPower(lStick);
+//        } else if (lStick>0) {
+//            motorFR.setPower(lStick);
+//        } else {
+//            motorFR.setPower(0);
+//        }
+        if (gamepad1.a) {
+            encoder(2);
+        }
+    }
 
-        if (gamepad1.dpad_up){
-            motor.setPower(1);
-        }
-        if (gamepad1.dpad_down) {
-            motor.setPower(-1);
-        }
+    public void encoder(int turnage) {
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        newTarget = ticks/turnage;
+        motorFR.setTargetPosition((int)newTarget);
+        motorFR.setPower(0.3);
+        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
 }
